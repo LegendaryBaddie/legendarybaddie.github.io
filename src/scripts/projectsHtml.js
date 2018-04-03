5074d45b69e48a69e9302fce3f2cb133adb36cb9
Austin White (RIT Student) <arw2013@ad.rit.edu>
2018-04-03 10:27:19 -0400

    initial commit

---
+++ src/scripts/projectsHtml.js
@@ -0,0 +1,4 @@
+export const pongHTML ="<img height=250px width=400px>
+                        <h3>Networked 6 Player Pong</h3>
+                        <p>This project was developed in node.js, and has</p>",
+puttonHTML, audioVizHTML, tugOWarHTML; 
